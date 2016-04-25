2fb5e6f0f72a436053936639718960d99a228618
Ak-basketball <kashyapanshulp@gmail.com>
2016-04-24 21:41:43 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+                        $("h1").hide();
+                        $("h3").hide();
+                        $("content").hide();
+    
+                        $("h1").fadeIn(1000);
+                        $("p").fadeIn(1000);
+                        $("content").fadeIn(1000);
+                  });
+          var wow = new WOW().init();
