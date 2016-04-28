5590df20dfbd90d7a04fe39382132e91fa991d73
Ak-basketball <kashyapanshulp@gmail.com>
2016-04-28 09:54:03 -0700

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -3,4 +3,10 @@ $(".menu-icon").click(function() {
     $(this).toggleClass("ak");
     $(".menuPage").toggleClass("appear");
 });
+$("h1").hide();
+$("h3").hide();
+
+$("h1").fadeIn(1000);
+$("h3").fadeIn(1000);
+  
 });
